e969e24635dee2b6ea25a2d3cba89a0a65a758c1
Anders Yu <andersyu31415@gmail.com>
2021-03-18 05:29:14 +0000

    Added steps to download keyboard for c.keyboard

--- acknowledgements.py
+++ acknowledgements.py
@@ -20,8 +20,8 @@ class Acknowledgements(commands.Cog):
   
   @bot.command (name='keyboard', help='Sends a file to download the Cocánb keyboard (Windows)')
   async def keyboard(self, ctx):
-    with open("Cocánb.klc", "rb") as file:
-      await ctx.send("Cocánb keyboard", file=discord.File(file, "Cocánb.klc"))
+    with open("cocanb.rar", "rb") as file:
+      await ctx.send("How to download keyboard (Windows 10):\nStep 1. Download the file below\nStep 2. Unzip the file\nStep 3. Run \"setup.exe\" and follow required steps\nStep 4. Go to Settings > Time & Language > Language > Add preferred language\nStep 5. Download English (Canada) keyboard\nStep 6. After the download has finished, click on English (Canada) in the preferred languages list and select \"Options\"\nStep 7. Click\"Add a keyboard\" and choose \"Cocánb Keyboard\"", file=discord.File(file, "Cocánb Keyboard"))
   
   @bot.command (name='bot', help='Sends a link to add this bot')
   async def bot(self, ctx):
