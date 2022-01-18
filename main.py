d771083b496db88af2b23ca7cc8cd4d11e2359b6
Altort Cocánb <77822790+cocanb-altort@users.noreply.github.com>
2022-01-18 05:49:09 +0000

    fuck you again grs

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ async def on_message(message):
     await message.channel.send("cringe")
   
   if message.author.id == 787576039093043202:
-    await message.channel.send(f"Fuck u, <@{message.author.id}> <3. I hope you die a horrible death on this cursed piece of shit Discord channel. lolollooloololool @here")
+    await message.channel.send("The Industrial Revolution and its consequences have been a disaster for the human race. They have greatly increased the life-expectancy of those of us who live in “advanced” countries, but they have destabilized society, have made life unfulfilling, have subjected human beings to indignities, have led to widespread psychological suffering (in the Third World to physical suffering as well) and have inflicted severe damage on the natural world. The continued development of technology will worsen the situation. It will certainly subject human beings to greater indignities and inflict greater damage on the natural world, it will probably lead to greater social disruption and psychological suffering, and it may lead to increased physical suffering even in “advanced” countries.")
 
 
 @bot.command(name='ping', help="Checks whether bot is online.")
