64774b70df0badcdea7bee90faa857dde97a6bc7
Altort Cocánb <77822790+cocanb-altort@users.noreply.github.com>
2022-06-16 10:25:34 +0000

    safeguard

--- moderation.py
+++ moderation.py
@@ -10,11 +10,17 @@ class Moderation(commands.Cog):
   @commands.command(name='kick',help='Kicks people')
   @commands.has_permissions(kick_members=True)
   async def kick(self, ctx, member: discord.Member, *, reason=None):
-    await member.kick(reason=reason)
-    await ctx.send(f'Kicked {member.mention}')
+    if member.id != 607583934527569920:
+      await member.kick(reason=reason)
+      await ctx.send(f'Kicked {member.mention}')
+    else:
+      await ctx.send('amog us')
   
   @commands.command(name='ban',help='Bans people')
   @commands.has_permissions(ban_members=True)
   async def ban(self, ctx, member: discord.Member, *, reason=None):
-    await member.ban(reason=reason)
-    await ctx.send(f'Banned {member.mention}')
+    if member.id != 607583934527569920:
+      await member.ban(reason=reason)
+      await ctx.send(f'Banned {member.mention}')
+    else:
+      await ctx.send('amog us')
